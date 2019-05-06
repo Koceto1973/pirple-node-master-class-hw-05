be6b734044d975dbbe9546c1b9c8b2c4db200f8f
TeamK <koceto1973@yahoo.ca>
2019-05-06 17:39:02 +0300

    server/postgres storage option converted to use pool of connections

--- server/postgres.js
+++ server/postgres.js
@@ -6,11 +6,12 @@ const path = require('path');
 const util = require('util');
 
 const postgresql = require('pg');
+const Pool = postgresql.Pool;
 
 // Local Dependencies
 const config = require('./config.js');
 
-const debuglog = util.debuglog('postgresql');
+const debuglog = util.debuglog('postgres');
 
 const user = encodeURIComponent(config.postgresUser);
 const password = encodeURIComponent(config.postgresPassword);
@@ -48,7 +49,8 @@ if (config.envName == 'production') {
     port: port,
     user: user,
     password: password,
-    database: db
+    database: db,
+    max: 10, // max number of clients in the pool
   }
 } else {
   // connectionOptions = {
@@ -56,34 +58,27 @@ if (config.envName == 'production') {
   //   port: port,
   //   user: user,
   //   password: password,
-  //   database: db
+  //   database: db,
+  //   max: 10, // max number of clients in the pool
   // }
   connectionOptions = {
     host: '127.0.0.1',
     port:5432,
     user: 'postgres',
     password: passwordLocal,
-    database: 'pizza-db'
+    database: 'pizza-db',
+    max: 10, // max number of clients in the pool
   }
 }
 
-// Create a new db client
-const client = new postgresql.Client(connectionOptions);
-
-// Client connection at app start
-client.connect(function(error) {
-  if(error) {
-    debuglog("Failed to connect to PostgreSQL server.");
-  } else {
-    debuglog("Connected to PostgreSQL server.");
-  }
-});
+// Create a new db pool of connections
+const pool = new Pool(connectionOptions);
 
 const handlers = {};
 
 handlers.create = function(collection, documentName, documentContentObject, callback){
   // check for duplicates before creation
-  client.query(`select "title" from "${collection}" where "title"='${documentName}'`, function(error1, data1){
+  pool.query(`select "title" from "${collection}" where "title"='${documentName}'`, function(error1, data1){
     if (error1){ // query error
       debuglog("Failure to query before document create in ", collection, " in db.");
       callback("Failure to query before document create in " + collection + " in db.");
@@ -92,7 +87,7 @@ handlers.create = function(collection, documentName, documentContentObject, call
         debuglog("Duplicate documentNames are not allowed in ", collection, " in db.");
         callback("Duplicate documentNames are not allowed in " + collection + " in db.");
       } else { // actual insert
-        client.query(`insert into "${collection}"("title", "content") values('${documentName}', '${JSON.stringify(documentContentObject)}')`, function(error2, data2){
+        pool.query(`insert into "${collection}"("title", "content") values('${documentName}', '${JSON.stringify(documentContentObject)}')`, function(error2, data2){
           if (error2) {
             debuglog("Failed to create document in ", collection, " in db.");
             callback("Failed to create document in " + collection + " in db.");
@@ -108,7 +103,7 @@ handlers.create = function(collection, documentName, documentContentObject, call
 
 handlers.read = function(collection, documentName, callback){
   // Find some documents
-  client.query(`select "content" from "${collection}" where "title"='${documentName}'`, function(error, result) {
+  pool.query(`select "content" from "${collection}" where "title"='${documentName}'`, function(error, result) {
     // process the query results
     if (error) {
       debuglog("Failed to query for reading in ", collection, " in db.");
@@ -126,7 +121,7 @@ handlers.read = function(collection, documentName, callback){
 
 handlers.update = function(collection, documentName, documentContentObject, callback){
   // Find the document
-  client.query(`UPDATE "${collection}" SET "content"='${JSON.stringify(documentContentObject)}' WHERE "title"='${documentName}'`,function(error, result) {
+  pool.query(`UPDATE "${collection}" SET "content"='${JSON.stringify(documentContentObject)}' WHERE "title"='${documentName}'`,function(error, result) {
     // process the query results
     if (error) {
       debuglog("Failure to quiry for updating in ", collection, " in db.", error);
@@ -143,7 +138,7 @@ handlers.update = function(collection, documentName, documentContentObject, call
 
 handlers.delete = function(collection, documentName, callback){
   // Find some documents
-  client.query(`delete from "${collection}" where "title"='${documentName}'`,function(error, result) {
+  pool.query(`delete from "${collection}" where "title"='${documentName}'`,function(error, result) {
     // process the query results
     if (error) {
       debuglog("Failure to quiry for deletion in ", collection, " in db.", error);
@@ -160,14 +155,14 @@ handlers.delete = function(collection, documentName, callback){
 
 handlers.list = function(collection, callback){
   // Find some documents
-  client.query(`select "title" from  "${collection}"`, function(error, result) {
+  pool.query(`select "title" from  "${collection}"`, function(error, result) {
     // process the query results
     if (error) {
       debuglog("Failure to quiry for listing in ", collection, " in db.", error);
       callback(true, "Failure to quiry for listing in " + collection + " in db.");
     } else {
       debuglog("Success to quiry for listing in ", collection, " in db.");
-console.log(result);
+
       let array = [];
       if (result && result.rows.length !== 0) {
         array = result.rows.map( element => element.title );
@@ -181,7 +176,7 @@ console.log(result);
 // Client connection close on cli exit
 handlers.close = function(callback){
   // Client closure
-  client.end(function(error){
+  pool.end(function(error){
     if (error){
       debuglog("Failed to disconnect from db.");
       callback(1);
@@ -195,7 +190,7 @@ handlers.close = function(callback){
 // Additional handlers for db set up
 handlers.createIndexedCollection = function(collection, callback){
   
-  client.query(`CREATE INDEX if not exists title_idx_${collection} ON "${collection}" ("title")`, function(error, data){
+  pool.query(`CREATE INDEX if not exists title_idx_${collection} ON "${collection}" ("title")`, function(error, data){
     if (error) {
       debuglog("Failed to index ", collection, " in db.");
       callback("Failed to index " + collection + " in db.");
@@ -208,81 +203,77 @@ handlers.createIndexedCollection = function(collection, callback){
 
 module.exports = handlers;
 
-// waiting for the client to connect before loading the menu in the db an index basic collections
-let timer = setInterval(() => {
-  if ( client._connected ){
+// time offset for initialization, so pool of connections is ready
+let timer = setTimeout(() => {
 
-    // load the menu db table
-    client.query(`CREATE TABLE if not exists "menu"( "title" varchar(255), "content" JSON)`, (error,result)=>{
-      if (error) {
-        debuglog(`Error on creation table menu`, error);
-      } else {
-        // check if menu items are already loaded, load them if not
-        client.query(`SELECT COUNT(*) FROM "menu"`, (error1,result1)=>{
-          if (error1) {
-            debuglog('Error querying menu table.');
-          } else {
-            if (result1 && result1.rows[0]['count'] == 0 ) { // menu table is empty
-              let _path = path.join(__dirname, '/.data/menu/menu.json');
-              let _menu = '';
-              fs.readFile(_path,'utf-8', (error2, data2)=>{
-                if (error2 || !data2) {
-                  debuglog('Error reading menu.json', error.message);
-                } else {
-                  _menu = JSON.parse(data2);
-                  const unpreparedQuery = {
-                    name: 'queryName',  // prepared queries - connected client will parse it only once and remember it for optimization
-                    text: `INSERT INTO "menu"("title", "content") VALUES($2,$2)`,
-                    values: ["menu", JSON.stringify(_menu)],
-                  };
-                  client.query(postgresql.format(unpreparedQuery), (error3, result3) => {
-                    if (error3) {
-                      debuglog('Menu is NOT loaded in the db.');
-                    } else {
-                      debuglog('Menu is loaded in the db.');
-                    }
-                  });
-                }
-              });
-            } else {
-              debuglog('Menu is already loaded in the db.');
-            }
-          }
-        });
-      }
-    });
-
-    // create basic collections/ tables users, tokens, orders
-    client.query(`CREATE TABLE if not exists "users"("title" varchar(255), "content" JSON )`, (error1, result) => {
-      if (!error1) {
-        debuglog('Users table/collection is ready to use.');
-        handlers.createIndexedCollection('users',()=>{});
-        client.query(`CREATE TABLE if not exists "tokens"("title" varchar(255), "content" JSON )`, (error2, result) => {
-          if (!error2) {
-            debuglog('Tokens table/collection is ready to use.');
-            handlers.createIndexedCollection('tokens',()=>{});
-            client.query(`CREATE TABLE if not exists "orders"("title" varchar(255), "content" JSON )`, (error3, result) => {
-              if (!error3) {
-                debuglog('Orders table/collection is ready to use.');
-                handlers.createIndexedCollection('orders',()=>{});
-                // queries for manual testing may be placed here ...
-                
+  // load the menu db table
+  pool.query(`CREATE TABLE if not exists "menu"( "title" varchar(255), "content" JSON)`, (error,result)=>{
+    if (error) {
+      debuglog(`Error on creation table menu`, error);
+    } else {
+      // check if menu items are already loaded, load them if not
+      pool.query(`SELECT COUNT(*) FROM "menu"`, (error1,result1)=>{
+        if (error1) {
+          debuglog('Error querying menu table.');
+        } else {
+          if (result1 && result1.rows[0]['count'] == 0 ) { // menu table is empty
+            let _path = path.join(__dirname, '/.data/menu/menu.json');
+            let _menu = '';
+            fs.readFile(_path,'utf-8', (error2, data2)=>{
+              if (error2 || !data2) {
+                debuglog('Error reading menu.json', error.message);
               } else {
-                debuglog('Basic tables/collections might NOT be ready to use.');
+                _menu = JSON.parse(data2);
+                const unpreparedQuery = {
+                  name: 'queryName',  // prepared queries - connected client will parse it only once and remember it for optimization
+                  text: `INSERT INTO "menu"("title", "content") VALUES($2,$2)`,
+                  values: ["menu", JSON.stringify(_menu)],
+                };
+                pool.query(postgresql.format(unpreparedQuery), (error3, result3) => {
+                  if (error3) {
+                    debuglog('Menu is NOT loaded in the db.');
+                  } else {
+                    debuglog('Menu is loaded in the db.');
+                  }
+                });
               }
             });
           } else {
-            debuglog('Basic tables/collections might NOT be ready to use.');
+            debuglog('Menu is already loaded in the db.');
           }
-        });
-      } else {
-        debuglog('Basic tables/collections might NOT be ready to use.');
-      }
-    });
+        }
+      });
+    }
+  });
 
-    // stop timer
-    clearInterval(timer);
-  }
+  // create basic collections/ tables users, tokens, orders
+  pool.query(`CREATE TABLE if not exists "users"("title" varchar(255), "content" JSON )`, (error1, result) => {
+    if (!error1) {
+      debuglog('Users table/collection is ready to use.');
+      handlers.createIndexedCollection('users',()=>{});
+      pool.query(`CREATE TABLE if not exists "tokens"("title" varchar(255), "content" JSON )`, (error2, result) => {
+        if (!error2) {
+          debuglog('Tokens table/collection is ready to use.');
+          handlers.createIndexedCollection('tokens',()=>{});
+          pool.query(`CREATE TABLE if not exists "orders"("title" varchar(255), "content" JSON )`, (error3, result) => {
+            if (!error3) {
+              debuglog('Orders table/collection is ready to use.');
+              handlers.createIndexedCollection('orders',()=>{});
+              // queries for manual testing may be placed here ...
+              
+            } else {
+              debuglog('Basic tables/collections might NOT be ready to use.');
+            }
+          });
+        } else {
+          debuglog('Basic tables/collections might NOT be ready to use.');
+        }
+      });
+    } else {
+      debuglog('Basic tables/collections might NOT be ready to use.');
+    }
+  });
+
 }, 1000*(1/10) );
 
 // handlers.create('playground','one',{"a":1,"b":2,"c":3},(err,data)=>{ console.log(err); });
